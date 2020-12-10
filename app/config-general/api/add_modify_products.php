9da6f5e8201befdb797f5222b07ce3a2df8186c4
hugo0243 <hugo0243@users.noreply.github.com>
2020-12-10 18:46:27 -0500

    cambio pestaña productos de modulo productos a modulo config-general

---
+++ app/config-general/api/add_modify_products.php
@@ -0,0 +1,68 @@
+<?php
+
+include_once($_SERVER['DOCUMENT_ROOT'] . '/dirs.php');
+require_once DAO_PATH . "UserDao.php";
+require_once DAO_PATH . "ProductDao.php";
+
+// revisar si existe session
+session_start();
+header("Content-Type: application/json");
+
+if (isset($_SESSION["user"])) {
+  $user = unserialize($_SESSION["user"]);
+  $productDao = new ProductDao();
+  if (isset($_POST["optionProductos"])) {
+    if (isset($_POST["ref"]) && isset($_POST["producto"]) && isset($_POST["rentabilidad"])) {
+      if ($_POST["ref"] != "" || $_POST["producto"] != "") {
+        if ($productDao->findNumberProductsByCompany($user->getCompany()->getId()) <= $user->getCompany()->getLicensedProducts()) {
+          if ($_POST["optionProductos"] == "option1") {
+            $product = new Product();
+            $product->setName($_POST["producto"]);
+            $product->setRef($_POST["ref"]);
+            if(empty($_POST["rentabilidad"])){
+              $product->setRentabilidad($user->getCompany()->getProfitabilityMargin());
+            } else {
+              $product->setRentabilidad($_POST["rentabilidad"]);
+            }
+            $product->setIdCompany($user->getCompany()->getId());
+
+            if (isset($_POST["formOption"]) && $_POST["formOption"] == '1' &&
+                isset($_POST["prodId"]) && $_POST["prodId"] != '-1') {
+                $product->setId($_POST["prodId"]);
+                if($productDao->update($product) == true) {
+                  http_response_code(200);
+                } else {
+                  http_response_code(500);
+                }
+                return;
+            }
+
+            if ($productDao->save($product) > 0) {
+              http_response_code(201);
+            } else {
+              http_response_code(500);
+            }
+          } else {
+            $product = $productDao->findById($_POST["producto"]);
+            if ($productDao->saveOrUpdateRawMaterial($product, $_POST["materia"], $_POST["cantidad"]) > 0) {
+              http_response_code(200);
+            } else {
+              http_response_code(500);
+            }
+          }
+        }else{
+          http_response_code(403);
+        }
+      } else {
+        http_response_code(400);
+      }
+    } else {
+      http_response_code(400);
+    }
+  } else {
+    http_response_code(412);
+  }
+} else {
+  http_response_code(401);
+  exit;
+}
---
+++ app/config-general/api/get_materials_product.php
@@ -0,0 +1,34 @@
+<?php
+// revisar si existe session
+session_start();
+set_time_limit(300);
+include_once($_SERVER['DOCUMENT_ROOT'] . '/dirs.php');
+require_once DAO_PATH . "UserDao.php";
+require_once DAO_PATH . "ProductDao.php";
+
+
+header("Content-Type: application/json");
+
+if (isset($_SESSION["user"])) {
+    $user = unserialize($_SESSION["user"]);
+    $productDao = new ProductDao();
+    if (isset($_GET["id"])) {
+        $product = $productDao->findById($_GET["id"], false, false, true);
+        if (isset($_GET["dataTable"])) {
+            $response = new  stdClass();
+            $response->data = $product->getMaterials();
+            echo json_encode($response);
+            exit;
+        } else {
+            echo json_encode($product->getMaterials());
+            exit;
+        }
+    } else {
+        $response = new  stdClass();
+        $response->data = [];
+        echo json_encode($response);
+    }
+} else {
+    http_response_code(401);
+    exit;
+}
